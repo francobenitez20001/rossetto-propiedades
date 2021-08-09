a74191f5cf201111c4ba84f6a2c0c63d664f4dfe
Franco Benitez <francobenitez980@gmail.com>
2021-08-09 14:38:45 -0300

    update

--- config/index.js
+++ config/index.js
@@ -1,2 +1,2 @@
 export const API = 'https://api-rossetto.herokuapp.com/api';
-export const URL = 'https://rossetto-propiedades.vercel.app';
+export const URL = 'https://rossetto-propiedades-phi.vercel.app';
